1d25c94cea694fab5c5ba5b20304b4f4103562dd
Daniel Riddersporre <danielriddersporre@gmail.com>
2021-09-17 14:37:46 +0200

    Added views for albums and artists. Both should display songs related to one and other.

---
+++ ljudio/src/views/Album.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="album-wrapper">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ ljudio/src/views/Artist.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="artist-wrapper">
+        
+    </div>
+</template>
